2a1e56a423e784c7372e0db985f72ec9c8bf916a
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-25 12:31:59 +0100

    Add hashing password functionality & add comparePassword method

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -30,6 +31,17 @@ const userSchema = new mongoose.Schema({
         default: 'user'
     }
 });
+
+userSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    const isCorrectPassword = await bcrypt.compare(candidatePassword, this.password)
+    return isCorrectPassword
+}
 
 const User = mongoose.model('User', userSchema);
 
