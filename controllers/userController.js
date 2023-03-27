f1f11aeef1daaa7f52dc6093c0b6a705e060165d
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-27 02:45:17 +0200

    Add getAllUsers & getSingleUser routes

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const { BadRequestError, UnauthenticatedError, NotFoundError } = require('../errors');
+const { StatusCodes } = require('http-status-codes');
+
+const getAllUsers = async (req, res) => {
+    const users = await User.find({ role: 'user' }).select('-password');
+    res.status(StatusCodes.OK).json({ users });
+}
+const getSingleUser = async (req, res) => {
+    const { id } = req.params
+    const user = await User.findById(id).select('-password');
+    if (!user) {
+        throw new NotFoundError(`There is no user with id ${id}..`);
+    }
+    res.status(StatusCodes.OK).json({ user })
+}
+const showCurrentUser = async (req, res) => {
+    res.status(StatusCodes.OK).json({ msg: 'current user' })
+}
+const updateUser = async (req, res) => {
+    res.status(StatusCodes.OK).json(req.body)
+}
+const updateUserPassword = async (req, res) => {
+    res.status(StatusCodes.OK).json(req.body)
+}
+
+module.exports = {
+    getAllUsers,
+    getSingleUser,
+    showCurrentUser,
+    updateUser,
+    updateUserPassword
+}
