04d437f7d8b13fad030c96eea1e70cd1a8a6f684
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-30 05:53:35 +0200

    Create a review functionality

--- controllers/reviewControllers.js
+++ controllers/reviewControllers.js
@@ -1,4 +1,5 @@
 const Review = require('../models/Review');
+const Product = require('../models/Product');
 const { BadRequestError, NotFoundError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
@@ -11,7 +12,18 @@ const singleReview = async (req, res) => {
 }
 
 const createReview = async (req, res) => {
-    res.status(StatusCodes.CREATED).json({ msg: 'create a review' });
+    const { product: productId } = req.body;
+    const isValidProduct = await Product.findById(productId);
+    if (!isValidProduct) {
+        throw new NotFoundError(`No product is exist with id ${productId}`);
+    }
+    const alreadySubmitted = await Review.findOne({ user: req.user.id, product: productId });
+    if (alreadySubmitted) {
+        throw new BadRequestError('You have already submitted a review for this product');
+    }
+    req.body.user = req.user.id
+    const review = await Review.create(req.body);
+    res.status(StatusCodes.CREATED).json(review);
 }
 
 const updateReview = async (req, res) => {
