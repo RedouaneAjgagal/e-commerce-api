b23a98dac6f5af8d0638fcc2a7f025e7fb4c5783
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-04-07 17:29:45 +0200

    Additional security packages

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const connectDB = require('./db/connect');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet');
+const xssCleaner = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const cors = require('cors');
 
 // routes
 const authRouter = require('./routes/authRoutes');
@@ -23,20 +28,25 @@ const orderRouter = require('./routes/orderRoutes');
 // Middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+    standardHeaders: true,
+    legacyHeaders: false
+})
 
+app.set('trust proxy', 1);
+app.use(helmet());
+app.use(cors());
+app.use(xssCleaner());
+app.use(mongoSanitize());
 
-
-app.use(morgan('dev'))
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static('./public'));
 app.use(fileUpload());
 
-app.get('/', (req, res) => {
-    const { token } = req.signedCookies;
-    res.json(token);
-});
-
+app.use('/api/v1', limiter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
